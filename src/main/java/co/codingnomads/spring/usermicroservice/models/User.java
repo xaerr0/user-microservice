d35eee77f4118456b437bebbc94138989dda8cb5
Danny Brown <xaerr00@gmail.com>
2023-04-17 00:04:37 -0700

    first commit

---
+++ src/main/java/co/codingnomads/spring/usermicroservice/models/User.java
@@ -0,0 +1,33 @@
+package co.codingnomads.spring.usermicroservice.models;
+
+import lombok.*;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String fullName;
+}
