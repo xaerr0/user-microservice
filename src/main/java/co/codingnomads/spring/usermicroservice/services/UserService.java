112a04925209549e16cc53d1153bc966d1b46e39
Danny Brown <xaerr00@gmail.com>
2023-04-17 00:55:49 -0700

    UserService

---
+++ src/main/java/co/codingnomads/spring/usermicroservice/services/UserService.java
@@ -0,0 +1,45 @@
+package co.codingnomads.spring.usermicroservice.services;
+
+
+import co.codingnomads.spring.usermicroservice.models.User;
+import co.codingnomads.spring.usermicroservice.repos.UserRepository;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+
+    public User getUserById(Long id) {
+        Optional<User> optional;
+        if ((optional = userRepository.findById(id)).isEmpty()) {
+            return null;
+        } else {
+            return optional.get();
+        }
+    }
+
+    public User getUserByUsername(String username) {
+        return userRepository.findByUsername(username);
+    }
+
+    public User createNewUser(User newUser) {
+        newUser.setPassword(newUser.getPassword());
+        return userRepository.save(newUser);
+    }
+
+    public User updateUser(User updatedUser) {
+        User user = userRepository.findByUsername(updatedUser.getUsername());
+        BeanUtils.copyProperties(updatedUser, user);
+        return userRepository.save(user);
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
+}
